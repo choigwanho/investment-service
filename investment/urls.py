d4378c497dfeb78be44a9a027176d2c915d78cef
dev-choee <rhksgh99@gmail.com>
2022-09-17 17:52:31 +0900

    :sparkles: (rhksghsss/feature/invest_main) ã투ì   , í자 상ì  개발

--- investment/urls.py
+++ investment/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('account/<str:pk>', views.AccountView, name='account'), # 투자 화면
+    path('account-asset/<str:pk>', views.accountAssetView, name='account-asset'), # 투자 상세 화면
+    path('my-asset/', views.myAssetView, name='my-asset'), # 보유 종목 화면
 ]
--- investment/views.py
+++ investment/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from investment.models import Account, Asset, AssetGroup
+from investment.serializers import UserAccountSerializer, AccountSerializer, AssetSerializer, AssetGroupSerializer
+from django.contrib.auth import get_user_model
+
+
+@api_view(['GET'])
+def AccountView(request, pk):
+    '''
+    투자 화면
+    '''
+    user = get_user_model().objects.get(id=pk)
+    serializer = UserAccountSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def accountAssetView(request, pk):
+    '''
+    투자 상세 화면
+    '''
+    account = Account.objects.get(id=pk)
+    serializer = AccountSerializer(account, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def myAssetView(request):
+    '''
+    보유 종목 화면
+    '''
+    assets = AssetGroup.objects.all()
+    serializer = AssetGroupSerializer(assets, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def transferAmount1(request,pk):
+    '''
+    투자금 입금 Phase 1 API
+    '''
+
+    account = Account.objects.get(id=pk)
+    serializer = AccountSerializer(instance=account, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def transferAmount2(request,pk):
+    '''
+    투자금 입금 Phase 2 API
+    '''
+
+    account = Account.objects.get(id=pk)
+    serializer = AccountSerializer(instance=account, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
